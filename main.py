2f2820b3a1edb9d1100d7c24b0d2871a0cb973b5
jerb <jsilliman@protonmail.com>
2024-01-06 12:05:16 -0500

    feat: uninstaller

--- main.py
+++ main.py
@@ -14,15 +14,20 @@ window = tk.Tk()
 
 textbox = tk.Text()
 textbox.bind("<Key>", lambda e: "break")
-button = tk.Button(text="Install")
+install_button = tk.Button(text="Install")
+uninstall_button = tk.Button(text="Uninstall")
 
 
 def perform_install():
     global textbox
-    global button
+    global install_button
+    global uninstall_button
 
-    textbox.insert("end", "Fetching latest release from GitHub...\n")
     try:
+        if not os.path.isfile("isaac-ng.exe"):
+            raise Exception("Installation must be performed from the game's directory!")
+
+        textbox.insert("end", "Fetching latest release from GitHub...\n")
         with requests.get(
             "https://api.github.com/repos/TeamREPENTOGON/REPENTOGON/releases/latest",
             headers={
@@ -76,25 +81,111 @@ def perform_install():
             os._exit(0)
 
     except Exception as e:
-        print(e)
         textbox.insert("end", "Failed to install!\n" + str(e) + "\n")
-    button["state"] = "normal"
-    button["text"] = "Install"
+
+    install_button["state"] = "normal"
+    uninstall_button["state"] = "normal"
+    install_button["text"] = "Install"
+
+
+def perform_uninstall():
+    global install_button
+    global uninstall_button
+    global textbox
+
+    try:
+        files = [
+            "dsound.dll",
+            "freetype.dll",
+            "libzhl.dll",
+            "Lua5.4.dll",
+            "zhlREPENTOGON.dll",
+            "dsound.log",
+            "repentogon.log",
+            "zhl.log",
+            "resources/scripts/main_ex.lua",
+            "resources/scripts/enums_ex.lua",
+            "resources/scripts/pysireffer.py",  # we really don't need to ship this anymore, do we?
+            "resources/scripts/repentogon_extras",
+            "resources/scripts/repentogon_tests",
+            "resources/rooms/26.The Void_ex.stb",
+            "resources/shaders/coloroffset_gold_mesafix.fs",
+            "resources/shaders/coloroffset_gold_mesafix.vs",
+            "resources-repentogon",
+        ]
+
+        cleanup_dirs = ["resources/rooms", "resources/shaders"]
+
+        if not os.path.isfile("isaac-ng.exe"):
+            raise Exception(
+                "Uninstallation must be performed from the game's directory!"
+            )
+            return
+
+        for file in files:
+            try:
+                if os.path.isfile(file):
+                    os.remove(file)
+                    textbox.insert("end", f"Successfully removed {file}\n")
+                elif os.path.isdir(file):
+                    shutil.rmtree(file)
+                    textbox.insert("end", f"Successfully removed directory {file}\n")
+
+            except FileNotFoundError:
+                pass
+
+        for directory in cleanup_dirs:
+            if os.path.isdir(directory) and not os.listdir(directory):
+                shutil.rmtree(directory)
+                textbox.insert("end", f"{directory} empty, removing\n")
+
+        textbox.insert("end", "Finished!")
+
+    except Exception as e:
+        textbox.insert("end", "Failed to uninstall!\n" + str(e) + "\n")
+
+    install_button["state"] = "normal"
+    uninstall_button["state"] = "normal"
+    uninstall_button["text"] = "Uninstall"
 
 
 def install():
-    global button
-    button["state"] = "disabled"
-    button["text"] = "Installing..."
+    global install_button
+    global uninstall_button
+    global textbox
+
+    install_button["state"] = "disabled"
+    uninstall_button["state"] = "disabled"
+    install_button["text"] = "Installing..."
+    textbox.delete(1.0, tk.END)
     t = threading.Thread(target=perform_install, daemon=True)
+    t.start()
+
+
+def uninstall():
+    global install_button
+    global uninstall_button
+    global textbox
+
+    install_button["state"] = "disabled"
+    uninstall_button["state"] = "disabled"
+    uninstall_button["text"] = "Uninstalling..."
+    textbox.delete(1.0, tk.END)
+
+    t = threading.Thread(target=perform_uninstall, daemon=True)
     t.start()
 
 
 window.title("REPENTOGON Installer")
 
 tk.Label(text="Welcome to the REPENTOGON Installer!").pack()
-button["command"] = install
-button.pack()
+
+install_button["command"] = install
+install_button.pack()
+
+uninstall_button["command"] = uninstall
+uninstall_button.pack()
+
 textbox.pack()
 
 if len(sys.argv) > 1 and "-auto" in sys.argv:
